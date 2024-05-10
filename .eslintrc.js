bfc0e2c1949cd44f8218550eab2ac75a6cc32a09
Gustav Rasmussen <gcr84@hotmail.com>
2024-05-10 17:54:03 +0200

    chore: update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,4 @@
 module.exports = {
-	env: {
-		"browser": true,
-		"es2021": true
-	},
 	root: true,
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
@@ -10,26 +6,5 @@ module.exports = {
 		'eslint:recommended',
 		'plugin:@typescript-eslint/eslint-recommended',
 		'plugin:@typescript-eslint/recommended'
-	],  // "standard-with-typescript",
-	overrides: [
-		{
-			"env": {
-				"node": true
-			},
-			"files": [
-				".eslintrc.{js,cjs}"
-			],
-			"parserOptions": {
-				"sourceType": "script"
-			}
-		}
-	],
-	parserOptions: {
-		"ecmaVersion": "latest",
-		"sourceType": "module",
-		project: "tsconfig.json",
-		allowJs: true
-	},
-	rules: {
-	}
+	]
 }
